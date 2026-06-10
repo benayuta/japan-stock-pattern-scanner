1448a590857afbff03fda58ddda5b8a57430de74
benayuta <benayuta@gmail.com>
2026-06-10 23:54:01 +0900

    Update tickers_loader.py

--- src/tickers_loader.py
+++ src/tickers_loader.py
@@ -1,16 +1,26 @@
 import csv
+import os
 
 
 def load_tickers():
 
     tickers = {}
 
-    with open("data/topix500.csv", encoding="utf-8") as f:
+    csv_path = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "data",
+        "topix500.csv"
+    )
+
+    with open(csv_path, encoding="utf-8") as f:
 
         reader = csv.DictReader(f)
 
         for row in reader:
 
-            tickers[row["ticker"]] = row["name"]
+            tickers[
+                row["ticker"]
+            ] = row["name"]
 
     return tickers
