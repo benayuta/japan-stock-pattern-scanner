d7c73f05a1506cb95b77944cd031c23191d7acb3
benayuta <benayuta@gmail.com>
2026-06-12 23:59:55 +0900

    Update fundamentals.py

--- src/fundamentals.py
+++ src/fundamentals.py
@@ -1,1 +1,36 @@
+import yfinance as yf
 
+
+def get_fundamentals(ticker):
+
+    try:
+
+        info = yf.Ticker(ticker).info
+
+        dividend_yield = info.get(
+            "dividendYield",
+            None
+        )
+
+        if dividend_yield is not None:
+            dividend_yield = round(
+                dividend_yield * 100,
+                2
+            )
+
+        ex_dividend = info.get(
+            "exDividendDate",
+            None
+        )
+
+        return {
+            "yield": dividend_yield,
+            "month": ex_dividend
+        }
+
+    except Exception:
+
+        return {
+            "yield": None,
+            "month": None
+        }
